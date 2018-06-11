c7c9f6b3e524ad5e91f2121e8486928310d69b9f
SylvainThibault <sthibault63@gmail.com>
2018-06-11 15:28:11 +0200

    Base commune

--- js/Board.js
+++ js/Board.js
@@ -1,21 +1,20 @@
 class Board {
-    constructor(tileSize, size) {
-        let tiles =
-
+    constructor(tileSize, boardSize) {
+        this.tilesArray = createTiles(boardSize);
         let distanceFromTop = 0;
         let distanceFromLeft = 0;
         let col, row;
 
         $('#main').empty().css({
-            'height': (tileSize * size) + "px",
-            'width': (tileSize * size) + "px"
+            'height': (tileSize * boardSize) + "px",
+            'width': (tileSize * boardSize) + "px"
         });
 
-        for (row = 0; row < size; row++) {
-            for (col = 0; col < size; col++) {
-                let tileNumber = size * row + col;
+        for (row = 0; row < boardSize; row++) {
+            for (col = 0; col < boardSize; col++) {
+                let tileNumber = boardSize * row + col;
                 $('#main').append(
-                    "<div class=\"tile\" id=" + tileNumber + "> " + tiles[tileNumber].text + "</div>"
+                    "<div class=\"tile\" id=" + tileNumber + "> " + this.tilesArray[row][col].text + "</div>"
                 );
                 $('#' + tileNumber).css({
                     'top': distanceFromTop + "px",
@@ -23,12 +22,25 @@ class Board {
                     'height': tileSize + "px",
                     'width': tileSize + "px"
                 });
-
+            if(this.tilesArray[row][col].isEmpty){
+                $('#' + tileNumber).css('background-color','red');
+            }
                 distanceFromLeft += tileSize;
             }
             distanceFromLeft = 0;
             distanceFromTop += tileSize;
         }
     }
+}
 
+function createTiles(boardSize) {
+    let tilesArray = [];
+    for (let row = 0; row < boardSize; row++) {
+        tilesArray[row] = [];
+        for (let col = 0; col < boardSize; col++) {
+            tilesArray[row][col] = new Tile(String.fromCharCode(65 + boardSize * row + col), false);
+        }
+    }
+    tilesArray[boardSize - 1][boardSize - 1].isEmpty = true;
+    return tilesArray;
 }
